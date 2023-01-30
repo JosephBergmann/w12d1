4ea8b31b5cced3033cb1711130edf24449c5bdc7
Joey Tsui <joeytsui131@gmail.com>
2023-01-30 14:43:32 -0500

    phase 1

---
+++ pokedex-rails-backend/app/models/poke_move.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: poke_moves
+#
+#  id         :bigint           not null, primary key
+#  pokemon_id :bigint
+#  move_id    :bigint
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class PokeMove < ApplicationRecord
+    belongs_to :pokemon,
+        foreign_key: :pokemon_id,
+        class_name: :Pokemon
+
+    belongs_to :move,
+        foreign_key: :move_id,
+        class_name: :Move
+
+end
